4e75c0a72aafb8760fc8fada6a5d8869fd8c8ef6
Peter Grunde <peter.a.grunde@gmail.com>
2014-07-01 09:31:38 -0600

    completed capybara tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,27 @@
-require "capybara/rspec"
+require 'capybara/rspec'
 require "./rsvp_app"
 
 Capybara.app = RsvpApp
+
+require "spec_helper"
+
+feature "Homepage" do
+  scenario "User can dollop a pwop" do
+    visit "/"
+
+    expect(page).to have_content("RSVP App")
+
+    click_link "RSVP"
+    expect(page).to have_content("Name")
+
+    fill_in("Name", :with => "Peter")
+    fill_in("Email", :with => "Peter.a.grunde@gmail.com")
+    fill_in("Email", :with => "Peter.a.grunde@gmail.com")
+    select "2", from: "Number of Guests"
+    choose("dinner_fish")
+
+    click_button "RSVP"
+    expect(page).to have_content("RSVPing")
+
+  end
+end
